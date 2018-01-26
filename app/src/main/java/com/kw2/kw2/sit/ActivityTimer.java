36848702196802ab0d664fd8b2bbab0b7870070c
KW2 <dksk73@naver.com>
2018-01-26 21:12:55 +0900

    Timer - logic finish

--- app/src/main/java/com/kw2/kw2/sit/ActivityTimer.java
+++ app/src/main/java/com/kw2/kw2/sit/ActivityTimer.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by SAMSUNG on 2018-01-21.
@@ -15,13 +19,28 @@ import android.widget.TextView;
 
 public class ActivityTimer extends Activity {
 
-    private boolean isPaused = false;
-
-    private boolean isCanceled = false;
+    boolean bRunning = true;
 
     private int nowSet = 1;
 
-    private long timeRemaining = 0;
+    private int nCounter = 0;
+
+    private int setNum = 0;
+
+    private int readyTimes = 5;
+
+    private int workTimes = 0;
+    private int wkValue = 0;
+
+    private int restTimes = 0;
+    private int rtValue = 0;
+
+    TimerTask mTimerTask;
+    final Handler handler = new Handler();
+    Timer t = new Timer();
+
+    TextView timerName, timeValueText, setValue, currentState, nowText;
+    Button spBtn, stopBtn, soundBtn, updateBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,79 +52,67 @@ public class ActivityTimer extends Activity {
         final int id = getIntent().getExtras().getInt("id");
         ListViewItem timer = helper.getItem(id);
 
-        final TextView timerName = (TextView) findViewById(R.id.timer_title);
-        final TextView timeValueText = (TextView) findViewById(R.id.timer_timeValue);
-        final TextView setValue = (TextView) findViewById(R.id.timer_setValue);
-        TextView currentState = (TextView) findViewById(R.id.timer_currentState);
-        TextView nowText = (TextView) findViewById(R.id.timer_nowText);
+        setNum = timer.getSetNum();
 
-        final Button updateBtn = (Button) findViewById(R.id.timer_updateBtn);
-        final Button spBtn = (Button) findViewById(R.id.timer_spBtn);
-        final Button stopBtn = (Button) findViewById(R.id.timer_stopBtn);
+        workTimes = stringToInt(timer.getWorkTime());
+        wkValue = workTimes;
+
+        restTimes = stringToInt(timer.getRestTime());
+        rtValue = restTimes;
+
+
+        timerName = (TextView) findViewById(R.id.timer_title);
+        timeValueText = (TextView) findViewById(R.id.timer_timeValue);
+        setValue = (TextView) findViewById(R.id.timer_setValue);
+        currentState = (TextView) findViewById(R.id.timer_currentState);
+        nowText = (TextView) findViewById(R.id.timer_nowText);
+
+        updateBtn = (Button) findViewById(R.id.timer_updateBtn);
+        spBtn = (Button) findViewById(R.id.timer_spBtn);
+        stopBtn = (Button) findViewById(R.id.timer_stopBtn);
+        soundBtn = (Button) findViewById(R.id.timer_soundBtn);
 
 
         timerName.setText(timer.getTimeName());
         timeValueText.setText(timer.getTimeValue());
-        setValue.setText(nowSet + " / " + timer.getSetNum());
+        setValue.setText(nowSet + " / " + setNum);
 
-        stopBtn.setEnabled(false);
-        spBtn.setTag(0, "start");
+        spBtn.setTag("start");
 
         // 시작/일시정지/재시작 버튼 클릭
         spBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(view.getTag(0).equals("start")){
+                updateBtn.setEnabled(true);
+                if(view.getTag().equals("start")){
                     // 시작 상태
-                    /*isPaused = false;
-                    isCanceled = false;
+                    if (mTimerTask == null) {
+                        bRunning = true;
+                        doTimerTask();
+                        spBtn.setText("||");
+                        spBtn.setTag("pause");
+                    }
 
-                    spBtn.setTag(0, "pause");
-                    updateBtn.setEnabled(false);
-
-                    stopBtn.setEnabled(true);
-
-                    CountDownTimer timer;
-                    long millisInFuture = 30 * 1000; //30 seconds
-                    long countDownInterval = 1000; //1 second
-
-
-                    //Initialize a new CountDownTimer instance
-                    timer = new CountDownTimer(millisInFuture,countDownInterval){
-                        public void onTick(long millisUntilFinished){
-                            //do something in every tick
-                            if(isPaused || isCanceled)
-                            {
-                                //If the user request to cancel or paused the
-                                //CountDownTimer we will cancel the current instance
-                                cancel();
-                            }
-                            else {
-                                //Display the remaining seconds to app interface
-                                //1 second = 1000 milliseconds
-                                tView.setText("" + millisUntilFinished / 1000);
-                                //Put count down timer remaining time in a variable
-                                timeRemaining = millisUntilFinished;
-                            }
-                        }
-                        public void onFinish(){
-                            //Do something when count down finished
-                            tView.setText("Done");
-
-                            //Enable the start button
-                            btnStart.setEnabled(true);
-                            //Disable the pause, resume and cancel button
-                            btnPause.setEnabled(false);
-                            btnResume.setEnabled(false);
-                            btnCancel.setEnabled(false);
-                        }
-                    }.start();*/
-
-                }else if(view.getTag(0).equals("pause")){
+                }else if(view.getTag().equals("pause")){
                     // 일시정지 상태
-                }else if(view.getTag(0).equals("resume")){
+                    doTimerPause();
+                    spBtn.setText("▶");
+                    spBtn.setTag("resume");
+
+                }else if(view.getTag().equals("resume")){
                     // 재시작 상태
+                    doTimerPause();
+                    spBtn.setText("||");
+                    spBtn.setTag("pause");
                 }
+            }
+        });
+
+        // 멈춤 버튼 클릭
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopTask();
             }
         });
 
@@ -121,9 +128,12 @@ public class ActivityTimer extends Activity {
 
                         ListViewItem item = helper.getItem(id);
 
+                        setNum = item.getSetNum();
+
                         timerName.setText(item.getTimeName());
                         timeValueText.setText(item.getTimeValue());
-                        setValue.setText(nowSet + " / " + item.getSetNum());
+                        setValue.setText(nowSet + " / " + setNum);
+
                     }
                 });
 
@@ -131,4 +141,103 @@ public class ActivityTimer extends Activity {
             }
         });
     }
+
+
+    public void doTimerTask() {
+
+        mTimerTask = new TimerTask() {
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        if(!bRunning) {
+                            return;
+                        }
+
+                        if(nCounter <= 4){
+                            nowText.setText("00:" + DialogMaker.twoStrForm(readyTimes--));
+                        }else if(nCounter <= 4 + workTimes){
+                            currentState.setText(R.string.workTime);
+                            nowText.setText(intToString(wkValue));
+                            wkValue--;
+                            if(nCounter == 4 + workTimes && nowSet == setNum){
+                                stopTask();
+                            }
+                        }else if(nCounter <= 4 + workTimes + restTimes){
+                            currentState.setText(R.string.restTime);
+                            nowText.setText(intToString(rtValue));
+                            rtValue--;
+                        }else if(nCounter > 4 + workTimes + restTimes){
+                            nowSet++;
+                            wkValue = workTimes;
+                            rtValue = restTimes;
+                            nCounter = 5;
+                            setValue.setText(nowSet + " / " + setNum);
+                            nowText.setText(intToString(wkValue));
+                            currentState.setText(R.string.workTime);
+                            wkValue--;
+                        }
+
+                        nCounter++;
+
+                    }
+                });
+            }
+        };
+
+        // public void schedule (TimerTask task, long delay, long period)
+        t.schedule(mTimerTask, 0, 1000); //
+
+    }
+
+    public void doTimerPause() {
+        bRunning = !bRunning;
+    }
+
+    public void stopTask() {
+
+        if (mTimerTask != null) {
+            updateBtn.setEnabled(false);
+            nowSet = 1;
+            readyTimes = 5;
+            wkValue = workTimes;
+            rtValue = restTimes;
+            setValue.setText(nowSet + " / " + setNum);
+            nowText.setText("00:05");
+
+            nCounter = 0;
+            bRunning = false;
+            mTimerTask.cancel();
+            mTimerTask = null;
+            spBtn.setText("▶");
+            spBtn.setTag("start");
+        }
+    }
+
+    public int stringToInt(String timeValue){
+        int intTimeValue = 0;
+
+        int mInt = Integer.parseInt(timeValue.substring(0,2)) * 60;
+        int sint = Integer.parseInt(timeValue.substring(3));
+
+        intTimeValue = mInt + sint;
+
+        return intTimeValue;
+    }
+
+    public String intToString(int intTimeValue){
+        String timeValue = "00:00";
+
+        String mString = "00";
+        String sString = "00";
+
+        if(intTimeValue != 0){
+            mString = String.valueOf( DialogMaker.twoStrForm(intTimeValue / 60) );
+            sString = String.valueOf( DialogMaker.twoStrForm(intTimeValue % 60) );
+        }
+
+        timeValue = mString + ":" + sString;
+
+        return timeValue;
+    }
+
 }
